175d9714a9261f600aaee5ac7ebf54dce609f633
Chien An Chen <annec987@gmail.com>
2018-03-15 20:37:17 +0800

    sample

--- src/Main4.java
+++ src/Main4.java
@@ -4,8 +4,6 @@ import static java.util.Comparator.*;
 public class Main4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 	
 	public static Comparator<Car> createComparator() {
